9ad3f95bb44b709117a71c9fa522d3c2c10805d0
Jero Lambre <jerolambre@gmail.com>
2023-05-29 14:36:40 -0300

    Initial Commit

---
+++ Code/Entregable3/Entregable3/main.c
@@ -0,0 +1,19 @@
+/*
+ * Entregable3.c
+ *
+ * Created: 29/05/2023 02:33:35 p. m.
+ * Author : Sharks
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
